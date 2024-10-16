8e70c0af9dc71e8c981fc1d03500fc4335470c71
aleishao9 <owusuboahenea@mymacewan.ca>
2024-10-16 03:08:33 +0000

    message

--- lab05/src/base2dec.c
+++ lab05/src/base2dec.c
@@ -9,16 +9,15 @@ int main(int argc, char* argv[])
     char num_str[10];
     int num_base = atoi(argv[2]);
     strcpy(num_str,argv[1]);
-    int to_decimal;
-    int num;
+    int to_decimal=0, k=strlen(num_str);
 
-    for(int i=0,k;i<=strlen(num_str);i++)
+    for(int i=0, num; i<k ;i++)
     {
-        k=strlen(num_str);
-        num=atoi(num_str[i]);
-        printf("Binary: %d, Base: %d, Exp: %d\n",num, num_base,k-i);
-        to_decimal=+(num)*(pow(num_base,k-i));
-        /*
+        num=num_str[i] - '0';
+        to_decimal += num * pow(num_base,k-i-1);
+        printf("Binary: %d, Base: %d, Exp: %d, Decimal: %d\n",num, num_base, k-i-1, to_decimal);
+    }
+            /*
         if(isalpha(num_str[0]))
         {
             if(strcmp(num_str[0],"A"))
@@ -28,7 +27,6 @@ int main(int argc, char* argv[])
             }
         }
         printf("%d", num_str[0]); */
-    }
-    printf("%d", to_decimal);
+    printf("%d\n", to_decimal);
     return 0;
 }
