28bef7a9564047a851dfc2b9cbb451f882fc99eb
aleishao9 <owusuboahenea@mymacewan.ca>
2024-10-29 03:26:32 +0000

    message

--- lab07/src/test_pointerFun.c
+++ lab07/src/test_pointerFun.c
@@ -4,28 +4,28 @@
 
 int main()
 {	
-    printf("Q1\n");
-    double coef[]={3.0,4.0}, x[]={1.0,2.0,3.0};
+    //FIX Q1_______________
+    /*printf("Q1\n");
+    double coef[]={7.0,28.0}, x[]={2.5,12.0,871.0};
 	int nc, na;
 	nc=sizeof(coef)/sizeof(coef[0]);
 	na=sizeof(x)/sizeof(x[0]);
     double y[na];
-    poly(coef, x, y, &nc, &na);
-    printf("\n \n");
+    poly(coef, x, y, &nc, &na);*/
 
 
-    printf("Q2\n");
-    char str[]="Pineapple";
+    /*printf("Q2\n");
+    char str[]="Pokemon";
     int n=sizeof(str)/sizeof(str[0]);
     reverso(str, &n);
-    printf("Reversed string: %s\n \n",str);
+    printf("Reversed string: %s\n \n",str);*/
 
 
-    printf("Q3\n");
-    char centr_str[]="The   ";
+    /*printf("Q3\n");
+    char centr_str[]="Snorkel                                                                                                                                                                                                                                                        ";
     int size=sizeof(centr_str)/sizeof(centr_str[0]);
     str_centre(centr_str, &size);
-    printf("Centered string: %s\n", centr_str);
+    printf("Centered string:%s\n", centr_str);*/
 
     return 0;
 }
