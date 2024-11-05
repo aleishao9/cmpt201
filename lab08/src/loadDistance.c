f9a20a233742ea5cad22f49c41582a9ef4b5d454
aleishao9 <owusuboahenea@mymacewan.ca>
2024-11-05 21:56:08 +0000

    message

--- lab08/src/loadDistance.c
+++ lab08/src/loadDistance.c
@@ -4,12 +4,12 @@
 
 int main()
 {
-    int n, current=1, read_lines=3;
+    int n, current=1, target=3;
     double* distance;
     FILE* fd;
     char buffer[100];
     int* ptr_x, ptr_y, ptr_z;
-    typedef struct {double* point1; double* point2; double* d} dist;
+    typedef struct {double* x; double* y; double* z; double* d} point;
     
     ptr_x,ptr_y,ptr_z= malloc(4 * sizeof(int));
 
@@ -21,22 +21,25 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    while(fgets(buffer, sizeof(buffer), fd))
+    while(current<target && fgets(buffer, sizeof(buffer), fd))
     {
-        if(current==read_lines)
-        {
-
-        }
+        current++;
     }
 
-    int dist_eq(ptr_x, ptr_y, ptr_z, distance)
+    for(int i=4; i>=target; i++)
     {
-        for(int i=0; i < 5; i++)
+        fscanf(fd,"%lf, %lf, %lf", ptr_x, ptr_y, ptr_z);
+        printf(ptr_x,ptr_y,ptr_z);
+    }
+}
+
+int dist_eq(ptr_x, ptr_y, ptr_z, distance)
+{
+    for(int i=0; i < 5; i++)
+    {
+        for(int j=1; j < 4; j++)
         {
-            for(int j=1; i < 4; j++)
-            {
-                *distance=pow((pow(ptr_x[i]-ptr_x[j])+pow(ptr_y[i]-ptr_y[j])+pow(ptr_z[i]-ptr_z)),(1/2));
-            }
+            distance=pow((pow(ptr_x-ptr_x,2)+pow(ptr_y-ptr_y,2)+pow(ptr_z-ptr_z,2)),(1/2));
         }
     }
 }
