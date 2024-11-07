241b5f7ac98ffe92ac8b654e566da8e47cf15da1
aleishao9 <owusuboahenea@mymacewan.ca>
2024-11-07 01:00:34 +0000

    message

--- lab08/src/loadDistance.c
+++ lab08/src/loadDistance.c
@@ -2,44 +2,75 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main()
-{
-    int n, current=1, target=3;
-    double* distance;
-    FILE* fd;
-    char buffer[100];
-    int* ptr_x, ptr_y, ptr_z;
-    typedef struct {double* x; double* y; double* z; double* d} point;
-    
-    ptr_x,ptr_y,ptr_z= malloc(4 * sizeof(int));
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Point;
 
+typedef struct {
+    int pointA; // Line index of the first point
+    int pointB; // Line index of the second point
+    double d;   // Calculated minimum distance between pointA and pointB
+} dist;
 
-    fd=fopen("data.dat","r");
-    if(fd==NULL)
-    {
-        printf("File not valid");
-        exit(EXIT_FAILURE);
-    }
-
-    while(current<target && fgets(buffer, sizeof(buffer), fd))
-    {
-        current++;
-    }
-
-    for(int i=4; i>=target; i++)
-    {
-        fscanf(fd,"%lf, %lf, %lf", ptr_x, ptr_y, ptr_z);
-        printf(ptr_x,ptr_y,ptr_z);
-    }
+// Function to calculate the Euclidean distance between two points
+double calculateDistance(Point a, Point b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2) + pow(a.z - b.z, 2));
 }
 
-int dist_eq(ptr_x, ptr_y, ptr_z, distance)
-{
-    for(int i=0; i < 5; i++)
+int main() {
+    FILE *file = fopen("data.dat", "r");
+    if (file == NULL) {
+        printf("File could not be opened.\n");
+        return 1;
+    }
+
+    /* to skip the first lines */
+    char buffer[256];
+    fgets(buffer, sizeof(buffer), file);
+    fgets(buffer, sizeof(buffer), file);
+
+    int n;
+    fscanf(file, "%d", &n);
+
+    /* malloc returns a 'void' pointer normally so in this case i'm type casting it
+    as a 'Point' pointer so that it can be treated as an array of Point structures*/
+    Point *points = (Point *)malloc(n * sizeof(Point));
+
+    /* assigning each Point it's x,y,z coordinates*/
+    for (int i = 0; i < n; i++)
     {
-        for(int j=1; j < 4; j++)
-        {
-            distance=pow((pow(ptr_x-ptr_x,2)+pow(ptr_y-ptr_y,2)+pow(ptr_z-ptr_z,2)),(1/2));
+        fscanf(file, "%f,%f,%f", &points[i].x, &points[i].y, &points[i].z);
+    }
+    fclose(file);
+
+    /* setting the initial distance to a very large number so that it can
+    be replaced by whatever distance that's calculated and i don't have code
+    an exeption for when distance empty or set at 0 to begin with */
+    dist minDist;
+    minDist.d = __DBL_MAX__;
+
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            double distance = calculateDistance(points[i], points[j]);
+            if (distance < minDist.d) {
+                minDist.d = distance;
+                /* updating current points with points
+                that have a smaller distance between them */
+                minDist.pointA = i + 1;
+                minDist.pointB = j + 1;
+            }
         }
     }
+
+
+    printf("The minimum distance is %.2f and is between points %d and %d.\n",
+           minDist.d, minDist.pointA, minDist.pointB);
+
+
+    free(points);
+
+    return 0;
 }
